edef049784cfa22d5cb1cb21718aa61c4e3f78f4
mats363 <81287846+mats363@users.noreply.github.com>
2021-12-22 17:59:00 +0100

    final

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ let time = setInterval(function(){
     let m = n.getMinutes();
     let h = n.getHours();
 
-    clock.innerHTML = h + ":" + m + ":" + s;
+    let time = formatTime(h, m, s);
+
+    clock.innerHTML = time;
     
     
 }, 1000);
 
+function formatTime(h, m, s) {
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    return h + ":" + m + ":" + s;
+    
+    
+}
